2f993add72bf613a0a599900e5dd3c2cfa2a2737
Maiza Teixeira dos Santos <maizasantos.dev@gmail.com>
2024-04-17 00:07:29 -0300

    start project

---
+++ oil-change-system/src/main/java/br/com/oilchangesystem/OilChangeSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.oilchangesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilChangeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilChangeSystemApplication.class, args);
+	}
+
+}
---
+++ oil-change-system/src/test/java/br/com/oilchangesystem/OilChangeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oilchangesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilChangeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
