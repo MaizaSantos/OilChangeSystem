1e06cdcce89ca806cb688f64c8d6721a6018c33b
Erick Dourado <erickdourado_@hotmail.com>
2024-05-07 22:21:12 -0300

    fix date field that broke after CORS config

--- oil-change-system/src/main/java/br/com/oilchangesystem/model/entity/ServiceRecord.java
+++ oil-change-system/src/main/java/br/com/oilchangesystem/model/entity/ServiceRecord.java
@@ -1,5 +1,6 @@
 package br.com.oilchangesystem.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.annotations.SourceType;
@@ -26,6 +27,7 @@ public class ServiceRecord {
     @JoinColumn
     private Service service;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
     @CurrentTimestamp(source = SourceType.VM)
     private LocalDateTime date;
 
